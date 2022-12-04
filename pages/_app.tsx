e93692c0826f74827521cdffc69de61eea5c236a
kaisic1224 <hiddenshadows1224@gmail.com>
2022-12-03 20:24:49 -0500

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { DefaultSeo } from "next-seo";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <DefaultSeo
+        openGraph={{
+          type: "website",
+          locale: "en_IE",
+          url: "https://www.url.ie/",
+          siteName: "SiteName"
+        }}
+        titleTemplate='Graphs | %s'
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
